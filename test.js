4fb7b5ea61cbb3bf9f2cdc8ae60b4b57ffec06fd
hashrock <hashedrock@gmail.com>
2020-01-12 16:04:44 +0900

    init

---
+++ test.js
@@ -0,0 +1,14 @@
+import { encode } from "./encoder.js";
+
+const data = [0x00, 0x00, 0x00, 0x00]
+const width = 1
+const height = 1
+
+var bmpData = {
+    data, //Buffer
+    width, //Number
+    height //Number
+};
+var rawData = encode(bmpData); //defaults to no compression
+
+await Deno.writeFile("./image.bmp", rawData);
