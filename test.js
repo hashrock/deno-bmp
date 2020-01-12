ad30bc44546e676d7f13da1a56a3e7e7601d0d2f
hashrock <hashedrock@gmail.com>
2020-01-12 17:03:12 +0900

    update example

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 import { encode } from "./encoder.js";
 
-const data = [0xFF, 0xFF, 0x00, 0x00]
-const width = 1
-const height = 1
+const data = []
 
-var bmpData = {
+const width = 300
+const height = 300
+
+for(let y = 0; y < height; y++){
+  for(let x = 0; x < width; x++){
+    let color = x / 300 * 0xff
+    let pixel = [
+      0xFF, Math.ceil(color), 0x00, 0x00
+    ]
+    data.push(...pixel)
+  }
+}
+
+// console.log(data)
+
+const bmpData = {
     data, //Buffer
     width, //Number
     height //Number
 };
-var rawData = encode(bmpData); //defaults to no compression
+const rawData = encode(bmpData); //defaults to no compression
 await Deno.writeFile("./image.bmp", new Uint8Array(rawData.data));
